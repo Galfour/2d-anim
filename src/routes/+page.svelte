aac761dcaa1cce7b2c9d4cdb19414f3be98e736f
gabe <ga@conjecture.dev>
2025-05-17 18:28:27 +0100

    tmp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,14 +29,33 @@
     cy : 0 ,
   } ;
 
+  let leftArmGroup = {
+    x : 5 ,
+    y : -8 ,
+    angle : 65 ,
+  } ;
+
   let leftArm = {
     fill : '#aaaaff' ,
     rx : 2 ,
     width : 10 ,
     height : 4 ,
-    x : 5 ,
+  } ;
+
+  let leftForeArm = {
+    fill : '#aaaabb' ,
+    rx : 2 ,
+    width : 10 ,
+    height : 4 ,
+    x : 7 ,
+    y : 0 ,
+    angle : 10 ,
+  } ;
+
+  let rightArmGroup = {
+    x : -1 ,
     y : -8 ,
-    angle : 65 ,
+    angle : 75 ,
   } ;
 
   let rightArm = {
@@ -44,42 +63,76 @@
     rx : 2 ,
     width : 10 ,
     height : 4 ,
-    x : -2 ,
-    y : -8 ,
-    angle : 65 ,
+  } ;
+
+  let rightForeArm = {
+    fill : '#5555aa' ,
+    rx : 2 ,
+    width : 10 ,
+    height : 4 ,
+    x : 7 ,
+    y : 0 ,
+    angle : 10 ,
+  } ;
+
+  let leftLegGroup = {
+    x : 2 ,
+    y : 5 ,
+    angle : 10 ,
   } ;
 
   let leftLeg = {
     fill : '#ffaaaa' ,
-    rx : 2 ,
+    rx : 3 ,
+    x : -2 ,
     width : 15 ,
-    height : 4 ,
-    x : 5 ,
-    y : 5 ,
-    angle : 80 ,
+    height : 5 ,
+  } ;
+
+  
+
+  let leftForeLeg = {
+    fill : '#ff8888' ,
+    rx : 3 ,
+    width : 13 ,
+    height : 4.5 ,
+    x : 9 ,
+    y : 0.5 ,
+    angle : 10 ,
   } ;
 
   let rightLeg = {
-    fill : '#ff5555' ,
-    rx : 2 ,
+    fill : '#cc5555' ,
+    rx : 3 ,
     width : 15 ,
-    height : 4 ,
+    height : 5 ,
     x : -2 ,
     y : 5 ,
     angle : 80 ,
   } ;
 </script>
 
-<svg width="400" height="400" viewBox="0 0 100 100" fill="red">
+<svg width="800" height="800" viewBox="0 0 100 100" fill="red">
 	<rect width="100" height="100" rx="25" fill="gray"/>
 
   <!-- body, just a container group -->
   <g transform={`translate(${body.x}, ${body.y})`}>
-    <!-- left arm (one rounded rectangle) -->
-    <rect {...leftArm} transform={`rotate(${leftArm.angle}, ${leftArm.x}, ${leftArm.y})`}/>
 
-    <!-- left leg (one rounded rectangle) -->
-    <rect {...leftLeg} transform={`rotate(${leftLeg.angle}, ${leftLeg.x}, ${leftLeg.y})`}/>
+    <!-- left arm group (two rounded rectangles) -->
+    <g transform={`rotate(${leftArmGroup.angle} , ${leftArmGroup.x}, ${leftArmGroup.y}) translate(${leftArmGroup.x}, ${leftArmGroup.y})`}>
+      <rect {...leftArm}/>
+      <rect {...leftForeArm} transform={`rotate(${-1 * leftForeArm.angle}, ${leftForeArm.x + leftForeArm.rx}, ${leftForeArm.y + leftForeArm.height / 2})`}/>
+    </g>
+
+    <!-- left leg group (two rounded rectangles) -->
+    <g transform={`rotate(${-leftLegGroup.angle + 90} , ${leftLegGroup.x}, ${leftLegGroup.y + leftLeg.height / 2}) translate(${leftLegGroup.x}, ${leftLegGroup.y})`}>
+      <rect {...leftLeg}/>
+      <rect {...leftForeLeg} transform={`rotate(${leftForeLeg.angle}, ${leftForeLeg.x }, ${leftForeLeg.y + leftForeLeg.height / 2})`}/>
+
+      <!-- debug purpose (rotation center fore leg) -->
+      <circle cx={leftForeLeg.x} cy={leftForeLeg.y + leftForeLeg.height / 2} r="1" fill="red"/>
+
+    </g>
 
     <!-- bust (one ellipse) -->
     <ellipse {...bust} />
@@ -92,94 +145,30 @@
 
 
     <!-- right arm (one rounded rectangle) -->
-    <rect {...rightArm} transform={`rotate(${rightArm.angle}, ${rightArm.x}, ${rightArm.y})`}/>
+    <g transform={`rotate(${rightArmGroup.angle} , ${rightArmGroup.x}, ${rightArmGroup.y}) translate(${rightArmGroup.x}, ${rightArmGroup.y})`}>
+      <rect {...rightArm}/>
+      <rect {...rightForeArm} transform={`rotate(${-1 * rightForeArm.angle}, ${rightForeArm.x + rightForeArm.rx}, ${rightForeArm.y + rightForeArm.height / 2})`}/>
+    </g>
 
 
 
     <!-- right leg (one rounded rectangle) -->
     <rect {...rightLeg} transform={`rotate(${rightLeg.angle}, ${rightLeg.x}, ${rightLeg.y})`}/>
+
   </g>
 
 </svg>
 
 <div style="display : flex; flex-direction : column; gap : 20px">
+  <div style="display : flex; flex-direction : row; gap : 20px">
+    <div>Left Fore Leg</div>
+    <div>
+      <div>Angle</div>
+      <input type="range" bind:value={leftForeLeg.angle} min="-90" max="90" step="0.1"/>
+      <div>{leftForeLeg.angle}</div>
+    </div>
 
-<div style="display : flex; gap : 10px">
-	<div>Head</div>
-	<div>
-		<div>Radius</div>
-		<input type="range" min="1" max="100" bind:value={head.circle.r}/>
-	</div>
-	<div>
-		<div>CX</div>
-		<input type="range" min="1" max="100" bind:value={head.x}/>
-	</div>
-	<div>
-		<div>CY</div>
-		<input type="range" min="1" max="100" bind:value={head.y}/>
-	</div>
-  <div>
-    <div>Color</div>
-    <input type="color" bind:value={head.fill}/>
-  </div>
-</div>
 
-<div style="display : flex; gap : 10px">
-  <div>Bust</div>
-  <div>
-    <div>RX</div>
-    <input type="range" min="1" max="100" bind:value={bust.rx}/>
   </div>
-  <div>
-    <div>RY</div>
-    <input type="range" min="1" max="100" bind:value={bust.ry}/>
-  </div>
-  <div>
-    <div>CX</div>
-    <input type="range" min="1" max="100" bind:value={bust.cx}/>
-  </div>
-  <div>
-    <div>CY</div>
-    <input type="range" min="1" max="100" bind:value={bust.cy}/>
-  </div>
-  <div>
-    <div>Color</div>
-    <input type="color" bind:value={bust.fill}/>
-  </div>
-</div>
-
-<div style="display : flex; gap : 10px">
-  <div>Left Arm</div>
-  <div>
-    <div>RX</div>
-    <input type="range" min="1" max="100" bind:value={leftArm.rx}/>
-  </div>
-</div>
-<!-- 
-<div style="display : flex; gap : 10px">
-  <div>Right Arm</div>
-  <div>
-    <div>RX</div>
-    <input type="range" min="1" max="100" bind:value={rightArm.rx}/>
-  </div>
-</div>
-
-<div style="display : flex; gap : 10px">
-  <div>Left Leg</div>
-  <div>
-    <div>RX</div>
-    <input type="range" min="1" max="100" bind:value={leftLeg.rx}/>
-  </div>
-</div>
-
-<div style="display : flex; gap : 10px">
-  <div>Right Leg</div>
-  <div>
-    <div>RX</div>
-    <input type="range" min="1" max="100" bind:value={rightLeg.rx}/>
-  </div>
-</div>
- -->
-
 
 </div>
